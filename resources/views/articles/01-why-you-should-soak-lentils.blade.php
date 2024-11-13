d9ce10e9c93c9572482ff7657b134bff6eca63e4
Aviran Abramov <aviranabramov@gmail.com>
2024-11-13 22:01:01 +0200

    Create few parts of a new article - soaking lentils

---
+++ resources/views/articles/01-why-you-should-soak-lentils.blade.php
@@ -0,0 +1,13 @@
+<x-layout metaTitle="Why You Should Soak Lentils">
+    <p>
+        Lentils are a one of the healthiest foods you can add to your diet. Packed with complex carbs, proteins, fibers, low in fat, and these legumes are also low in fat and calories. Plus, they bring a boost of calcium, phosphorus, iron, and B vitamins, making them one of the best foods, even for bodybuilders.
+    </p>
+
+    <p>
+        There are several versions of lentils, such as brown, green, yellow, black and red lentils that deliver healthy benefits to our bodies. Despite their healthy benefits, lentils can sometimes lead to issues like digestive discomfort, bloating and gas. However, there is an easy way to make lentils easier on our stomach: soaking them before cooking.
+    </p>
+
+    <p>
+        In just 100g of black lentils for example, they have: 26.7g of proteins, 60g of carbs, 1.1g of fats, 356 calories, and 14.5g of fibers!
+    </p>
+</x-layout>
