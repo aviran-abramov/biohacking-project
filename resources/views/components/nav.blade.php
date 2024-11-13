fc1f042c610a40f22bf4aba91da84f4d0654b048
Aviran Abramov <aviranabramov@gmail.com>
2024-11-13 21:18:09 +0200

    Add articles and products pages to the nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,6 +2,8 @@
     <ul>
         <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
         <x-nav-link href="/supplements" :active="request()->is('supplements')">Supplements</x-nav-link>
+        <x-nav-link href="/articles" :active="request()->is('articles')">Articles</x-nav-link>
+        <x-nav-link href="/products" :active="request()->is('products')">Products</x-nav-link>
         <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
     </ul>
 </nav>
