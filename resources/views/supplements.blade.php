a3697ddec29a42814824f725e76b21a08857655b
Aviran Abramov <aviranabramov@gmail.com>
2024-11-14 19:05:48 +0200

    Improve supplements and some semantics there

--- resources/views/supplements.blade.php
+++ resources/views/supplements.blade.php
@@ -1,15 +1,23 @@
 <x-layout metaTitle="Supplements">
-    <h1>This is the Supplements page</h1>
+    <header>
+        <h1 class="text-center font-bold text-4xl mt-4 mb-1">Supplements List</h1>
+        <h2 class="text-center font-semi text-2xl mb-4">Recommended by Experts</h2>
+    </header>
 
     {{-- Supplements Section --}}
-    <section class="flex gap-2 p-2">
+    <section class="flex gap-4 p-2">
         @foreach ($supplements as $supplement)
             {{-- Supplement Card --}}
-            <div class="flex flex-col border border-gray-300 rounded p-4 pt-3 gap-2 flex-1">
-                <div class="font-semibold text-md">{{ $supplement->name }}</div>
+            <article class="flex flex-col border border-gray-300 rounded p-4 pt-3 gap-2 flex-1">
+                <h3 class="font-semibold text-md">{{ $supplement->name }}</h3>
                 <div class="text-sm text-gray-500 font-medium">{{ $supplement->description }}</div>
-                <a class="mt-auto text-center border border-green-400 hover:bg-green-400 btn btn-sm bg-white" href="{{ $supplement->purchase_url }}" target="_blank">BUY IT NOW</a>
-            </div>
+
+                {{-- Supplement Links --}}
+                <div class="mt-auto flex flex-col gap-2">
+                    <a class="text-sm text-green-500 hover:underline" href="#">Read More</a>
+                    <a class="text-center border border-green-400 hover:bg-green-400 btn btn-sm bg-white" href="{{ $supplement->purchase_url }}" target="_blank">BUY IT NOW</a>
+                </div>
+            </article>
         @endforeach
     </section>
 </x-layout>
