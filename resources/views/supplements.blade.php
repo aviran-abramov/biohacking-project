ed6cc0658de482ff784a2dd7b3e077f08e9c4d08
Aviran Abramov <aviranabramov@gmail.com>
2024-11-14 18:26:03 +0200

    Improve supplements card a bit more

--- resources/views/supplements.blade.php
+++ resources/views/supplements.blade.php
@@ -1,13 +1,15 @@
 <x-layout metaTitle="Supplements">
     <h1>This is the Supplements page</h1>
 
-    <div class="flex gap-2 p-2">
+    {{-- Supplements Section --}}
+    <section class="flex gap-2 p-2">
         @foreach ($supplements as $supplement)
-            <div class="border border-gray-300 rounded p-2">
-                <div>{{ $supplement->name }}</div>
-                <div>{{ $supplement->description }}</div>
-                <a href="{{ $supplement->purchase_url }}">BUY IT NOW</a>
+            {{-- Supplement Card --}}
+            <div class="flex flex-col border border-gray-300 rounded p-4 pt-3 gap-2 flex-1">
+                <div class="font-semibold text-md">{{ $supplement->name }}</div>
+                <div class="text-sm text-gray-500 font-medium">{{ $supplement->description }}</div>
+                <a class="mt-auto text-center border border-green-400 hover:bg-green-400 btn btn-sm bg-white" href="{{ $supplement->purchase_url }}" target="_blank">BUY IT NOW</a>
             </div>
         @endforeach
-    </div>
+    </section>
 </x-layout>
