16649bfb6cc41cd7525e3bc28e8cb23a588305ce
Aviran Abramov <aviranabramov@gmail.com>
2024-11-14 14:17:29 +0200

    Add 3 supplements in the database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Supplement;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,27 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Supplement::create([
+            "name" => "Ashwagandha",
+            "description" => "Herb that promotes relaxation, helps with mental clarity, reduces stress and cortisol levels.",
+            "purchase_url" => "https://iherb.com/pr/youtheory-ashwagandha-60-vegetarian-capsules/90165"
+        ]);
+
+        Supplement::create([
+            "name" => 'Omega 3',
+            "description" => 'Supports heart health, improves brain function, reduces inflammation, supports mood, skin, and joint health',
+            "purchase_url" => "https://iherb.com/pr/sports-research-omega-3-fish-oil-triple-strength-120-softgels/109322"
+        ]);
+
+        Supplement::create([
+            "name" => 'Creatine Monohydrate',
+            "description" => 'Boosts muscle strength, enhances exercise performance, supports energy production, aids recovery, and improves muscle growth',
+            "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-sport-creatine-monohydrate-unflavored-1-lb-454-g/71026"
         ]);
     }
 }
