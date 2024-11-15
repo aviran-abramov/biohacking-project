7f3d5d798c1123cbe04cb3e4f7bb9e9768f2407b
Aviran Abramov <aviranabramov@gmail.com>
2024-11-15 09:56:15 +0200

    Put supplements array inside data folder

---
+++ database/seeders/data/supplements.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        "name" => "Ashwagandha",
+        "description" => "Herb that promotes relaxation, helps with mental clarity, reduces stress and cortisol levels.",
+        "purchase_url" => "https://iherb.com/pr/youtheory-ashwagandha-60-vegetarian-capsules/90165"
+    ],
+    [
+        "name" => "Omega 3",
+        "description" => "Supports heart health, improves brain function, reduces inflammation, supports mood, skin, and joint health.",
+        "purchase_url" => "https://iherb.com/pr/sports-research-omega-3-fish-oil-triple-strength-120-softgels/109322"
+    ],
+    [
+        "name" => "Creatine Monohydrate",
+        "description" => "Boosts muscle strength, enhances exercise performance, supports energy production, aids recovery, and improves muscle growth.",
+        "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-sport-creatine-monohydrate-unflavored-1-lb-454-g/71026"
+    ],
+    [
+        "name" => "Vitamin D3K2",
+        "description" => "Supports bone health, boosts immunity, improves calcium absorption, prevents artery calcification, and promotes heart health.",
+        "purchase_url" => "https://iherb.com/pr/sports-research-d3-k2-plant-based-60-veggie-softgels/79975"
+    ],
+    [
+        "name" => "Magnesium Glycinate",
+        "description" => "Improves sleep, reduces stress, supports muscle relaxation, boosts heart health, enhances mood, and promotes better digestion.",
+        "purchase_url" => "https://iherb.com/pr/solaray-high-absorption-magnesium-glycinate-350-mg-120-vegcaps/112971"
+    ],
+    [
+        "name" => "Apple Cider Vinegar",
+        "description" => "Aids digestion, supports weight management, balances blood sugar, boosts skin health, and promotes heart wellness.",
+        "purchase_url" => "https://iherb.com/pr/bragg-organic-apple-cider-vinegar-with-the-mother-16-fl-oz-473-ml/5817"
+    ]
+];
