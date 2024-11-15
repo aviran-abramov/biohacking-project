4caf4219ad64b1f628b8642fcf120d8e3d4089f8
Aviran Abramov <aviranabramov@gmail.com>
2024-11-15 09:33:17 +0200

    Organize supplement seeder with an array and foreach create

--- database/seeders/SupplementSeeder.php
+++ database/seeders/SupplementSeeder.php
@@ -13,40 +13,41 @@ class SupplementSeeder extends Seeder
      */
     public function run(): void
     {
-        Supplement::create([
-            "name" => "Ashwagandha",
-            "description" => "Herb that promotes relaxation, helps with mental clarity, reduces stress and cortisol levels.",
-            "purchase_url" => "https://iherb.com/pr/youtheory-ashwagandha-60-vegetarian-capsules/90165"
-        ]);
+        $supplements = [
+            [
+                "name" => "Ashwagandha",
+                "description" => "Herb that promotes relaxation, helps with mental clarity, reduces stress and cortisol levels.",
+                "purchase_url" => "https://iherb.com/pr/youtheory-ashwagandha-60-vegetarian-capsules/90165"
+            ],
+            [
+                "name" => "Omega 3",
+                "description" => "Supports heart health, improves brain function, reduces inflammation, supports mood, skin, and joint health.",
+                "purchase_url" => "https://iherb.com/pr/sports-research-omega-3-fish-oil-triple-strength-120-softgels/109322"
+            ],
+            [
+                "name" => "Creatine Monohydrate",
+                "description" => "Boosts muscle strength, enhances exercise performance, supports energy production, aids recovery, and improves muscle growth.",
+                "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-sport-creatine-monohydrate-unflavored-1-lb-454-g/71026"
+            ],
+            [
+                "name" => "Vitamin D3K2",
+                "description" => "Supports bone health, boosts immunity, improves calcium absorption, prevents artery calcification, and promotes heart health.",
+                "purchase_url" => "https://iherb.com/pr/sports-research-d3-k2-plant-based-60-veggie-softgels/79975"
+            ],
+            [
+                "name" => "Magnesium Glycinate",
+                "description" => "Improves sleep, reduces stress, supports muscle relaxation, boosts heart health, enhances mood, and promotes better digestion.",
+                "purchase_url" => "https://iherb.com/pr/solaray-high-absorption-magnesium-glycinate-350-mg-120-vegcaps/112971"
+            ],
+            [
+                "name" => "Apple Cider Vinegar",
+                "description" => "Aids digestion, supports weight management, balances blood sugar, boosts skin health, and promotes heart wellness.",
+                "purchase_url" => "https://iherb.com/pr/solaray-high-absorption-magnesium-glycinate-350-mg-120-vegcaps/112971"
+            ]
+        ];
 
-        Supplement::create([
-            "name" => "Omega 3",
-            "description" => "Supports heart health, improves brain function, reduces inflammation, supports mood, skin, and joint health.",
-            "purchase_url" => "https://iherb.com/pr/sports-research-omega-3-fish-oil-triple-strength-120-softgels/109322"
-        ]);
-
-        Supplement::create([
-            "name" => "Creatine Monohydrate",
-            "description" => "Boosts muscle strength, enhances exercise performance, supports energy production, aids recovery, and improves muscle growth.",
-            "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-sport-creatine-monohydrate-unflavored-1-lb-454-g/71026"
-        ]);
-
-        Supplement::create([
-            "name" => "Vitamin D3K2",
-            "description" => "Supports bone health, boosts immunity, improves calcium absorption, prevents artery calcification, and promotes heart health.",
-            "purchase_url" => "https://iherb.com/pr/sports-research-d3-k2-plant-based-60-veggie-softgels/79975"
-        ]);
-
-        Supplement::create([
-            "name" => "Magnesium Glycinate",
-            "description" => "Improves sleep, reduces stress, supports muscle relaxation, boosts heart health, enhances mood, and promotes better digestion.",
-            "purchase_url" => "https://iherb.com/pr/solaray-high-absorption-magnesium-glycinate-350-mg-120-vegcaps/112971"
-        ]);
-
-        Supplement::create([
-            "name" => "Apple Cider Vinegar",
-            "description" => "Aids digestion, supports weight management, balances blood sugar, boosts skin health, and promotes heart wellness.",
-            "purchase_url" => "https://iherb.com/pr/solaray-high-absorption-magnesium-glycinate-350-mg-120-vegcaps/112971"
-        ]);
+        foreach ($supplements as $supplement) {
+            Supplement::create($supplement);
+        }
     }
 }
