60ae421c1814f1b3adf145f66a23532f89c76d7c
Aviran Abramov <aviranabramov@gmail.com>
2024-11-15 16:52:36 +0200

    Add more data to the supplements list

--- database/seeders/data/supplements.php
+++ database/seeders/data/supplements.php
@@ -30,5 +30,80 @@ return [
         "name" => "Apple Cider Vinegar",
         "description" => "Aids digestion, supports weight management, balances blood sugar, boosts skin health, and promotes heart wellness.",
         "purchase_url" => "https://iherb.com/pr/bragg-organic-apple-cider-vinegar-with-the-mother-16-fl-oz-473-ml/5817"
-    ]
+    ],
+    [
+        "name" => "Collagen Peptides",
+        "description" => "Enhances skin elasticity, strengthen hair, improves joint health, supports bone density, and boosts muscle recovery.",
+        "purchase_url" => "https://iherb.com/pr/sports-research-collagen-peptides-unflavored-1-lb-454-g/71106"
+    ],
+    [
+        "name" => "Inulin",
+        "description" => "Supports gut health, improves digestion, promotes regularity, boosts beneficial gut bacteria, enhances calcium absorption, and stabilizes blood sugar.",
+        "purchase_url" => "https://iherb.com/pr/now-foods-certified-organic-inulin-prebiotic-pure-powder-8-oz-227-g/1100"
+    ],
+    [
+        "name" => "NAC",
+        "description" => "Supports liver detoxification, boosts antioxidant levels, improves lung health, enhances immunity, and promotes mental clarity.",
+        "purchase_url" => "https://iherb.com/pr/lake-avenue-nutrition-n-acetyl-l-cysteine-600-mg-120-veggie-capsules/97436"
+    ],
+    [
+        "name" => "Extra Virgin Olive Oil",
+        "description" => "Supports heart health, reduces inflammation, boosts brain function, enhances skin health, and promotes longevity.",
+        "purchase_url" => "https://iherb.com/pr/bragg-organic-extra-virgin-olive-oil-16-fl-oz-473-ml/110956"
+    ],
+    [
+        "name" => "NMN",
+        "description" => "Boosts cellular energy, supports DNA repair, enhances brain health, promotes longevity, and improves metabolic function.",
+        "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-nmn-powder-1-06-oz-30-g/112136"
+    ],
+    [
+        "name" => "Turmeric",
+        "description" => "Reduces inflammation, supports joint health, boosts immunity, enhances digestion, improves skin health, and promotes brain function.",
+        "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-superfoods-organic-turmeric-powder-4-oz-114-g/93007"
+    ],
+    [
+        "name" => "Zinc Citrate",
+        "description" => "Boosts immunity, supports wound healing, enhances skin health, improves digestion, promotes hormone balance, and aids enzyme function.",
+        "purchase_url" => "https://iherb.com/pr/21st-century-zinc-citrate-50-mg-60-tablets/104765"
+    ],
+    [
+        "name" => "Tongkat Ali",
+        "description" => "Enhances energy levels, boosts testosterone, improves mood, supports muscle growth, promotes libido, and reduces stress.",
+        "purchase_url" => "https://iherb.com/pr/solaray-tongkat-ali-400-mg-60-vegcaps/99983"
+    ],
+    [
+        "name" => "Magnesium L-Threonate",
+        "description" => "Improves cognitive function, enhances memory, supports brain health, reduces stress, promotes relaxation, and boosts overall mental clarity.",
+        "purchase_url" => "https://iherb.com/pr/now-foods-magtein-magnesium-l-threonate-90-veg-capsules/57577"
+    ],
+    [
+        "name" => "L-Theanine",
+        "description" => "Promotes relaxation, reduces stress, enhances focus, improves sleep quality, boosts mood, and supports cognitive performance.",
+        "purchase_url" => "https://iherb.com/pr/california-gold-nutrition-l-theanine-featuring-alphawave-200-mg-60-veggie-capsules/83280"
+    ],
+    [
+        "name" => "Apigenin",
+        "description" => "Promotes relaxation, supports sleep quality, reduces inflammation, boosts brain health, enhances mood, and supports cardiovascular health.",
+        "purchase_url" => "https://iherb.com/pr/swanson-apigenin-50-mg-90-capsules/116327"
+    ],
+    [
+        "name" => "Inositol",
+        "description" => "Supports mental health, improves mood, enhances hormonal balance, promotes metabolic health, aids fertility, and boosts overall brain function.",
+        "purchase_url" => "https://iherb.com/pr/now-foods-inositol-capsules-500-mg-100-veg-capsules/684"
+    ],
+    [
+        "name" => "Alpha-GPC",
+        "description" => "Enhances cognitive function, boosts memory, supports focus, promotes brain health, improves learning ability, and aids physical performance.",
+        "purchase_url" => "https://iherb.com/pr/evlution-nutrition-alpha-gpc-60-veggie-capsules/115828"
+    ],
+    [
+        "name" => "Glycine",
+        "description" => "Supports sleep quality, promotes collagen production, enhances brain health, boosts detoxification, aids muscle recovery, and improves digestion.",
+        "purchase_url" => "https://iherb.com/pr/now-foods-glycine-1-000-mg-100-veg-capsules/18106"
+    ],
+    [
+        "name" => "Taurine",
+        "description" => "Supports heart health, enhances energy production, boosts brain function, promotes hydration, reduces oxidative stress, and improves exercise performance.",
+        "purchase_url" => "https://iherb.com/pr/now-foods-taurine-double-strength-1-000-mg-100-veg-capsules/15787"
+    ],
 ];
